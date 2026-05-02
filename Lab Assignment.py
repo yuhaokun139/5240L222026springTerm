7fabd05d6361a3c98d9845b617eebfad86b49987
yuhaokun139 <georgehk139@gmail.com>
2026-05-02 16:26:27 +0800

    Update Lab Assignment.py

--- Lab Assignment.py
+++ Lab Assignment.py
@@ -7,13 +7,26 @@ def img2text(url):
 
 # text2story
 def text2story(text):
-    story_text = ""   # to be completed
+    story_pipe = pipeline("text-generation", 
+                          model="pranavpsv/genre-story-generator-v2")
+    story_text = story_pipe(scenario)
+    story = story_results[0]['generated_text']
+    st.write(f"**Story:** {story}")   
     return story_text
 
 # text2audio
 def text2audio(story_text):
-    audio_data = ""     # to be completed
+    audio_pipe = pipeline("text-to-audio", model="Matthijs/mms-tts-eng")
+    audio_data = audio_pipe(story)    
     return audio_data
 
 # main part
-# ... to be completed
+def main():
+    
+    if st.button("Play Audio"):
+        audio_array = audio_data["audio"]
+        sample_rate = audio_data["sampling_rate"]
+        st.audio(audio_array, sample_rate=sample_rate)
+        
+main()
+
