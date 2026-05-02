36f0d2d9e2a55cdd2336c726bf44d298710be7cc
yuhaokun139 <georgehk139@gmail.com>
2026-05-02 16:28:46 +0800

    Update Lab Assignment.py

--- Lab Assignment.py
+++ Lab Assignment.py
@@ -22,7 +22,16 @@ def text2audio(story_text):
 
 # main part
 def main():
+    st.set_page_config(page_title="Your Image to Audio Story", page_icon="🦜")
+    st.header("ISOM5240: Turn Your Image to Audio Story")
     
+    uploaded_file = st.file_uploader("Select an Image...")
+    
+    if uploaded_file is not None:
+# Save file locally
+        bytes_data = uploaded_file.getvalue()
+        with open(uploaded_file.name, "wb") as file:
+        file.write(bytes_data)
     if st.button("Play Audio"):
         audio_array = audio_data["audio"]
         sample_rate = audio_data["sampling_rate"]
