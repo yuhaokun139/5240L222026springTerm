0a6500f0ff02aaae9efe07e6d3810d735ab7766d
yuhaokun139 <georgehk139@gmail.com>
2026-05-02 16:58:41 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -25,26 +25,4 @@ def main():
         if st.button("Click Me"):
             st.write("🎉 You clicked the button!")
 
-
-from transformers import pipeline
-from PIL import Image
-
-# Streamlit UI
-print("Title: Age Classification using ViT")
-
-# Load the age classification pipeline
-# The code below should be placed in the main part of the program
-age_classifier = pipeline("image-classification",
-                          model="nateraw/vit-age-classifier")
-
-image_name = "middleagedMan.jpg"
-image_name = Image.open(image_name).convert("RGB")
-
-# Classify age
-age_predictions = age_classifier(image_name)
-print(age_predictions)
-age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
-
-# Display results
-print("Predicted Age Range:")
-print(f"Age range: {age_predictions[0]['label']}")
+main ()
